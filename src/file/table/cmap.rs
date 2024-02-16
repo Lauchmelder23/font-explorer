e52e38b84dd844e7ae5d3bf0a546a04789f62630
Robert <robert.altner@einweckglas.com>
2024-02-16 23:44:04 +0100

    add segmented glyph table mapping

--- src/file/table/cmap.rs
+++ src/file/table/cmap.rs
@@ -87,7 +87,7 @@ impl EncodingRecord {
     {
         match format {
             0 => EncodingRecord::get_byte_encoded_glyphs(length, buf),
-            4 => Ok(GlyphMap::new()),
+            4 => EncodingRecord::get_segment_encoded_glyphs(length, buf),
 
             _ => todo!()
         }
@@ -112,6 +112,66 @@ impl EncodingRecord {
 
                 Ok(())
             })?;
+
+        Ok(glyphs)
+    }
+
+    fn get_segment_encoded_glyphs<T>(_: u16, buf: &mut T) -> std::io::Result<GlyphMap> 
+        where T: Read + Seek
+    {
+        let segments: usize = (((buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as usize) << 8) |
+                            (buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as usize)) >> 1;
+
+        buf.seek(SeekFrom::Current(6))?;
+
+        let mut buffer = vec![0u8; segments as usize * 2];
+        
+        buf.read_exact(&mut buffer)?;
+        let end_codes: Vec<u16> = (0..segments).map(|i| u16::from_be_bytes(buffer[2*(i as usize)..2*(i as usize)+2].try_into().unwrap())).collect();
+
+        buf.seek(SeekFrom::Current(2))?;
+
+        buf.read_exact(&mut buffer)?;
+        let start_codes: Vec<u16> = (0..segments).map(|i| u16::from_be_bytes(buffer[2*(i as usize)..2*(i as usize)+2].try_into().unwrap())).collect();
+
+        buf.read_exact(&mut buffer)?;
+        let deltas: Vec<i16> = (0..segments).map(|i| i16::from_be_bytes(buffer[2*(i as usize)..2*(i as usize)+2].try_into().unwrap())).collect();
+
+        buf.read_exact(&mut buffer)?;
+        let offsets: Vec<u16> = (0..segments).map(|i| u16::from_be_bytes(buffer[2*(i as usize)..2*(i as usize)+2].try_into().unwrap())).collect();
+
+        let glyphs_start = buf.stream_position()?;
+        let mut glyphs = GlyphMap::new();
+
+        for range in 0..segments {
+            for codepoint in start_codes[range]..=end_codes[range] {
+                let glyph: u16;
+
+                if offsets[range] != 0 {
+                    let glyph_id = offsets[range] / 2 + (codepoint - start_codes[range]);
+                    buf.seek(SeekFrom::Start(glyphs_start + glyph_id as u64))?;
+
+                    let raw_glyph = ((buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as u16) << 8) |
+                                         (buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as u16);
+
+                    glyph = raw_glyph.wrapping_add_signed(deltas[range]);
+
+                } else {
+                    let glyph_id = (codepoint - start_codes[range]).wrapping_add_signed(deltas[range]);
+                    buf.seek(SeekFrom::Start(glyphs_start + glyph_id as u64))?;
+
+                    glyph = ((buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as u16) << 8) |
+                            (buf.bytes().next().unwrap_or(Err(new_error!("Unwrapping failed")))? as u16);
+
+                }
+
+                let Some(character) = char::from_u32(codepoint as u32) else {
+                    return Err(new_error!("Character conversion failed"));
+                };
+
+                glyphs.insert(character, glyph);
+            }
+        }
 
         Ok(glyphs)
     }
