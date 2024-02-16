b711d68b12f904049487d68d38af7d5dc2503e9b
Robert <robert.altner@einweckglas.com>
2024-02-16 20:30:38 +0100

    refactor encoding table extraction

--- src/file/table/cmap.rs
+++ src/file/table/cmap.rs
@@ -1,10 +1,28 @@
-use std::io::{Read, Seek};
+use std::io::{BufReader, Read, Seek};
 use super::{TableReader, TableRecord};
 
 #[derive(Debug)]
 pub struct CharacterGlyphMapping {
     version: u16,
     encodings: Vec<EncodingRecord>
+}
+
+impl CharacterGlyphMapping {
+    fn read_subtable<T>(data: &[u8], buf: &mut T, record: &TableRecord) -> std::io::Result<EncodingRecord>
+        where T: Seek {
+        let platform = u16::from_be_bytes(data[0..2].try_into().unwrap());
+        let encoding = u16::from_be_bytes(data[2..4].try_into().unwrap());
+        let offset = u32::from_be_bytes(data[4..8].try_into().unwrap());
+
+        let pos = record.offset + offset;
+        let next_entry_pos = buf.stream_position()?;
+
+        let encoding_record = EncodingRecord::new(platform, encoding, pos);
+
+        buf.seek(std::io::SeekFrom::Start(next_entry_pos))?;
+
+        Ok(encoding_record)
+    }
 }
 
 impl TableReader for CharacterGlyphMapping {
@@ -20,35 +38,17 @@ impl TableReader for CharacterGlyphMapping {
         let version = u16::from_be_bytes(header_buf[0..2].try_into().unwrap());
         let num_tables = u16::from_be_bytes(header_buf[2..4].try_into().unwrap());
 
-        let mut ret_val = CharacterGlyphMapping {
-            version, encodings: vec![]
-        };
-
         let mut records_data: Vec<u8> = vec![0; 8 * (num_tables as usize)];
         buf.read_exact(&mut records_data)?;
 
-        records_data = dbg!(records_data);
-
-        (0..num_tables)
+        let encodings = (0..num_tables)
             .map(|i| &records_data[8*(i as usize)..8*(i as usize)+8])
-            .try_for_each(|data| {
-                let platform = u16::from_be_bytes(data[0..2].try_into().unwrap());
-                let encoding = u16::from_be_bytes(data[2..4].try_into().unwrap());
-                let offset = u32::from_be_bytes(data[4..8].try_into().unwrap());
+            .map(|data| CharacterGlyphMapping::read_subtable(data, buf, record))
+            .collect::<std::io::Result<_>>()?;
 
-                let pos = record.offset + offset;
-                let next_entry_pos = buf.stream_position()?;
-
-                ret_val.encodings.push(
-                    EncodingRecord::new(platform, encoding, pos)
-                );
-
-                buf.seek(std::io::SeekFrom::Start(next_entry_pos))?;
-
-                Ok::<(), std::io::Error>(())
-            })?;
-
-        Ok(ret_val)
+        Ok(CharacterGlyphMapping {
+            version, encodings
+        })
     }
 }
 
